16b0564d493397e20191cfcf16f868777fcdf2b3
vibincholayil <vibinnath.c@gmail.com>
2025-05-20 17:12:39 +0100

    try commit first time

--- subnet.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.vpc_terraform_2.id
-  cidr_block        = var.public_subnet_cidrs[count.index]
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.vpc_terraform_2.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
-}
--- variables.tf
+++ variables.tf
@@ -10,15 +10,4 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"   
 }
 
-variable "public_subnet_cidrs" {
-  description = "A list of CIDR blocks for the public subnets."
-  type        = list(string)
-  default     = "10.0.1.0/24"
-}
-
-variable "private_subnet_cidrs" {
-  description = "A list of CIDR blocks for the private subnets."
-  type        = list(string)
-  default     = "10.0.2.0/24"
-}
 
